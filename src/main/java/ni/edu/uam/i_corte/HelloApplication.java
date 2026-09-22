c96fb00e93616d0766dfdf4529efe2f95872bab2
Rodolfo Collado <rodolfocollado20@gmail.com>
2026-09-22 16:14:33 -0600

    feat: create scene in HelloApplication

--- src/main/java/ni/edu/uam/i_corte/HelloApplication.java
+++ src/main/java/ni/edu/uam/i_corte/HelloApplication.java
@@ -11,8 +11,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 760, 520);
+        stage.setTitle("Agenda Salud | Citas médicas");
+        stage.setMinWidth(700);
+        stage.setMinHeight(480);
         stage.setScene(scene);
         stage.show();
     }
